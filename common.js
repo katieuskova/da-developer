ebb6fd47ca244b49286e92a88a1e3c7ec8a4968c
Ekaterina <katieuskova@gmail.com>
2020-05-15 17:38:43 +0300

    da-developer

---
+++ common.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+    let mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+            1120: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            769: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+        }
+    });
+});
+
+let $page = $('html, body');
+$('a[href*="#"]').click(function () {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+    return false;
+});
+$(document).ready(function () {
+
+    $('.btn')
+        .click(function () {
+            $('body').addClass('fixed');
+            $('.popup-container').fadeIn(1000);
+        });
+
+    $('.popup-container').click(function (event) {
+        if (event.target == this) {
+            $('body').removeClass('fixed');
+            $(this).fadeOut(100);
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('input[type="tel"]').inputmask({ "mask": "+7(999) 999-9999" });
+});
+
+$(document).ready(function () {
+    $('.popup__form').validate({
+        errorPlacement(error, element) {
+            return true;
+        },
+        rules: {
+            Имя: {
+                required: true,
+                minlength: 4,
+                maxlength: 16,
+            },
+
+            Телефон: {
+                required: true,
+            },
+
+            Согласие: {
+                required: true,
+            },
+        },
+        submitHandler: function (form) {
+            form.submit();
+            form.reset();
+        }
+    });
+
+
+    let burger = document.querySelector('.header__burger')
+    let menu = document.querySelector('.header__nav')
+    let navigation = document.querySelector('.nav__list')
+    burger.addEventListener('click', function (e) {
+        e.preventDefault();
+        menu.classList.toggle('header__nav-open');
+        navigation.classList.toggle('nav__list-open');
+        burger.classList.toggle('burger-active');
+    });
+});
+
+$(document).ready(function () {
+    var details = $('.details');
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= $(details).offset().top) {
+            $('.details__hashtag-1').addClass('hashtag-active-1');
+            $('.details__hashtag-2').addClass('hashtag-active-2');
+            $('.details__hashtag-3').addClass('hashtag-active-3');
+            $('.details__hashtag-4').addClass('hashtag-active-4');
+            $('.details__hashtag-5').addClass('hashtag-active-5');
+        }
+    });
+});
